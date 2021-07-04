423d209b3490c528422839851a0b11a76693daae
saransh vaid <saranshvaid8@gmail.com>
2021-07-04 15:51:47 -0400

    Added Links test

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,11 +1,16 @@
 package base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.util.List;
+
 
 public class BaseTests {
 
@@ -17,19 +22,32 @@ public class BaseTests {
     public void beforeTestSetUp(){
         System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(URL);
        }
 
      @AfterTest
      public void afterTestSetUp(){
-        driver.quit();
         driver.close();
+        driver.quit();
      }
 
     @Test
     public void verifyPageTitleIsDisplayed(){
-        driver.get(URL);
+
         String actualPageTitle = driver.getTitle();
         System.out.println(actualPageTitle);
         Assert.assertEquals(PAGE_TITLE, actualPageTitle);
+    }
+
+    @Test
+    public void verifyInputLinkIsFunctional(){
+        List<WebElement> linkElements = driver.findElements(By.tagName("a"));
+        System.out.println("The page contains" + linkElements.size() + "links");
+        WebElement inputLink = driver.findElement(By.linkText("Inputs"));
+        String inputLinkText = inputLink.getText();
+        inputLink.click();
+        String headerText = driver.findElement(By.tagName("h3")).getText();
+        Assert.assertTrue(inputLinkText.equalsIgnoreCase(headerText));
     }
 }
